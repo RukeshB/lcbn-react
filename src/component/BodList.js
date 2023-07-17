bc5038b92bb74b4191ad2e1a766fe22bd3f95923
RukeshB <rukeshbasukala@gmail.com>
2023-07-17 19:48:24 +0545

    fetch data

--- src/component/BodList.js
+++ src/component/BodList.js
@@ -1,6 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Bod from "./Bod";
 
 const BodList = () => {
+    const [bods, setBods] = useState([])
+
+    useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios
+      .get("https://sheet.best/api/sheets/9a29fb5c-8f3c-428b-a864-24fe269537f8")
+      .then((response) => {
+        setBods(response.data)
+      });
+  };
+
+  const display = bods.map((bod, key) => {
+    return (
+        <Bod
+        key={key}
+        image_url={bod.image_url}
+        name={bod.name}
+        position={bod.position}
+        ly="2023/24"
+        email={bod.email}
+      />
+    );
+  }
+  )
+
   return (
     <div>
       <div className="mt-4 md:mt-6">
@@ -8,34 +38,7 @@ const BodList = () => {
         <p className="text-sm text-gray-500 md:text-base">For LY 2023/24</p>
       </div>
       <div className="flex flex-wrap ">
-        <Bod
-          image_id="1OCJ2bpunCvvWPsqCm4-ohZdlYEcHoXiP"
-          name="Rukesh Basukala"
-          position="1st Vice president"
-          ly="2023/24"
-          email="rukeshbasukala@gmail.com"
-        />
-        <Bod
-          image_id="1OCJ2bpunCvvWPsqCm4-ohZdlYEcHoXiP"
-          name="Rukesh Basukala"
-          position="1st Vice president"
-          ly="2023/24"
-          email="rukeshbasukala@gmail.com"
-        />
-        <Bod
-          image_id="1OCJ2bpunCvvWPsqCm4-ohZdlYEcHoXiP"
-          name="Rukesh Basukala"
-          position="1st Vice president"
-          ly="2023/24"
-          email="rukeshbasukala@gmail.com"
-        />
-        <Bod
-          image_id="1OCJ2bpunCvvWPsqCm4-ohZdlYEcHoXiP"
-          name="Rukesh Basukala"
-          position="1st Vice president"
-          ly="2023/24"
-          email="rukeshbasukala@gmail.com"
-        />
+        {display}
       </div>
     </div>
   );
