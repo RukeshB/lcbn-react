894a3f6130f9edcaa21c59616468f5e134422a75
RukeshB <rukeshbasukala@gmail.com>
2023-07-17 17:12:38 +0545

    design Bod component

---
+++ src/component/Bod.js
@@ -0,0 +1,41 @@
+const Bod = (props) => {
+  return (
+    <div className="max-w-2xl mx-2 mt-16 text-gray-900 bg-white rounded-lg shadow-xl sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto">
+      <div className="h-32 overflow-hidden rounded-t-lg">
+        <img
+          className="object-cover object-top w-full"
+          src="https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+          alt="Mountain"
+        />
+      </div>
+      <div className="relative w-32 h-32 mx-auto -mt-16 overflow-hidden border-4 border-white rounded-full">
+        <img
+          className="object-cover object-center h-36"
+          src={`https://drive.google.com/thumbnail?id=${props.image_id}`}
+          alt={props.name}
+        />
+      </div>
+      <div className="mt-6 text-center">
+        <h2 className="text-xl font-semibold">Leo {props.name}</h2>
+        <p className="text-xl">{props.position}</p>
+        <p className="text-gray-500">for LY {props.ly}</p>
+      </div>
+      <ul className="flex flex-col items-center justify-around py-4 mt-6 text-gray-700">
+        <li className="flex items-center justify-around gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            className="w-4 text-blue-900 fill-current bi bi-envelope-at-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2H2Zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671Z" />
+            <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034v.21Zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791Z" />
+          </svg>
+          <div>{props.email}</div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Bod;
