bf56149ec71df132a88bb0a4abcaffb42ef81e9c
RukeshB <rukeshbasukala@gmail.com>
2023-07-17 18:55:16 +0545

    add components to list all bods

---
+++ src/component/BodList.js
@@ -0,0 +1,44 @@
+import Bod from "./Bod";
+
+const BodList = () => {
+  return (
+    <div>
+      <div className="mt-4 md:mt-6">
+        <p className="text-lg font-semibold md:text-xl">Board Of Directors</p>
+        <p className="text-sm text-gray-500 md:text-base">For LY 2023/24</p>
+      </div>
+      <div className="flex flex-wrap ">
+        <Bod
+          image_id="1OCJ2bpunCvvWPsqCm4-ohZdlYEcHoXiP"
+          name="Rukesh Basukala"
+          position="1st Vice president"
+          ly="2023/24"
+          email="rukeshbasukala@gmail.com"
+        />
+        <Bod
+          image_id="1OCJ2bpunCvvWPsqCm4-ohZdlYEcHoXiP"
+          name="Rukesh Basukala"
+          position="1st Vice president"
+          ly="2023/24"
+          email="rukeshbasukala@gmail.com"
+        />
+        <Bod
+          image_id="1OCJ2bpunCvvWPsqCm4-ohZdlYEcHoXiP"
+          name="Rukesh Basukala"
+          position="1st Vice president"
+          ly="2023/24"
+          email="rukeshbasukala@gmail.com"
+        />
+        <Bod
+          image_id="1OCJ2bpunCvvWPsqCm4-ohZdlYEcHoXiP"
+          name="Rukesh Basukala"
+          position="1st Vice president"
+          ly="2023/24"
+          email="rukeshbasukala@gmail.com"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BodList;
