05d6a6bf81c9c15f24277f7aea81f5f97cf69726
RukeshB <rukeshbasukala@gmail.com>
2023-07-19 21:50:17 +0545

    added image

--- src/component/Home/ImageSlider.js
+++ src/component/Home/ImageSlider.js
@@ -1,4 +1,6 @@
 import { Carousel } from "@material-tailwind/react";
+import image_1 from "../../assets/image/AMN04602.jpg"
+import image_2 from "../../assets/image/1658750357963.jpg"
 
 const ImageSlider = () => {
   return (
@@ -19,18 +21,13 @@ const ImageSlider = () => {
       )}
     >
       <img
-        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-        alt="1"
+        src={image_1}
+        alt="image_1"
         className="h-full w-full object-cover"
       />
       <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-        alt="2"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-        alt="3"
+        src={image_2}
+        alt="image_2"
         className="h-full w-full object-cover"
       />
     </Carousel>
