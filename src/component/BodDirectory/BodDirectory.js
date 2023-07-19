7a8d591dc49016e5648303f2622081063c578ab9
RukeshB <rukeshbasukala@gmail.com>
2023-07-19 20:38:13 +0545

    add footer text

--- src/component/BodDirectory/BodDirectory.js
+++ src/component/BodDirectory/BodDirectory.js
@@ -3,9 +3,10 @@ import BodList from "./BodList";
 
 const BodDirectory = () => {
   return (
-    <div>
+    <div className="md:my-8 my-4">
       <ClubDetails />
       <BodList />
+      <p className='mt-8 text-base md:text-lg bg-[#263A5F] text-white'>“ Inspiring growth and change ”</p>
     </div>
   );
 };
