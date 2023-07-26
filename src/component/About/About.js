cabf00524f4c85c7f2c0ef201fe071aa54d7a3e9
RukeshB <rukeshbasukala@gmail.com>
2023-07-26 14:50:38 +0545

    update content of about page

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -33,18 +33,7 @@ const About = () => {
           title="Bhaktapur Namaste Leo Club"
           description={
             <span className="text-xs md:text-base">
-              Leo clubs are a youth organization of Lions Clubs International
-              <br />
-              Leo clubs encourage youths to develop leadership qualities by
-              participating in social service activities. They are dependent on
-              a Lions club to sponsor and initiate a Leo club. Leo club members
-              are addressed as "Leos." They conduct various projects in the
-              fields of health care, elders, children, disabled people, literacy
-              and education, and self-development. Leos can raise funds by
-              conducting fund-raising projects. They can conduct projects with
-              another Leo club, sponsoring Lions club, or with an outside
-              organization. Leo clubs are sponsored by Lions clubs and comprise
-              an official program of Lions Clubs International.
+             Leo Club of Bhaktapur Namaste was chartered in 25th November, 2021 with Leo Babin Lama as a character president under guidance of Lions Club of Bhaktapur Namaste. Our primary objective is to foster personal development, motivate each other towards social services, and cultivate essential leadership skills and teamwork. Together, we aim to make a positive impact on our community and beyond, leaving a lasting legacy of service, compassion, and leadership. 
             </span>
           }
         />
