f4f26d2a045eed5b5e1d0efa8b80c00faf3dfc47
RukeshB <rukeshbasukala@gmail.com>
2023-07-26 14:51:08 +0545

    use Card component

--- src/component/Contact/ContactUser.js
+++ src/component/Contact/ContactUser.js
@@ -1,11 +1,15 @@
+import { Card } from "@material-tailwind/react";
 import UserCard from "./UserCard";
 
 const ContactUser = () => {
   return (
-    <div className="flex flex-col justify-center md:flex-row md:justify-evenly">
+    <Card className="max-w-screen-xl md:px-12 lg:px-16 xl:px-32 py-16 mx-auto">
+      <h1 className="text-xl font-semibold md:text-4xl md:font-bold my-4">For More Detail Contact</h1>
+      <div className="flex flex-col justify-center md:flex-row md:justify-evenly gap-8">
         <UserCard image_url="https://res.cloudinary.com/dmx53ztyl/image/upload/v1689602282/Leo%20club%20Bods/Leo_Rina_Pahiju_president_mco9ar.png" name="Rina Pahiju" position="President" email="rinapahiju@gmail.com" phone="9803605027"/>
         <UserCard image_url="https://res.cloudinary.com/dmx53ztyl/image/upload/v1689602280/Leo%20club%20Bods/Leo_Anit_Koju_Public_relation_officer_bcfc1o.png" name="Anit Koju" position="Public Relation Officer" email="anitkoju0@gmail.com" phone="9808287871"/>
-    </div>
+      </div>
+    </Card>
   );
 };
 
