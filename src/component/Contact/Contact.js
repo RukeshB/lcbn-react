9894fcba75e34087d32aaffe7cb3c87d344ff419
RukeshB <rukeshbasukala@gmail.com>
2023-07-26 15:14:11 +0545

    create message card for contact us

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -1,9 +1,11 @@
 import ContactUser from "./ContactUser";
+import Message from "./Message";
 
 const Contact = () => {
     return (
-        <div className="pt-32 md:px-24 px-1">
-            <h1 className="text-xl font-semibold md:text-4xl md:font-bold mb-4">Contact Us</h1>
+        <div className="pt-32 md:px-24 px-1 flex flex-col gap-8 mb-8">
+            <h1 className="text-xl font-semibold md:text-4xl md:font-bold">Contact Us</h1>
+            <Message/>
            <ContactUser/>
         </div>
     )
