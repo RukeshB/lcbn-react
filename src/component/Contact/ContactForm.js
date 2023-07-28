b9624952889cc910b8606d1f7e2d8e8d059dee65
RukeshB <rukeshbasukala@gmail.com>
2023-07-28 12:29:50 +0545

    - post input data - clear input field after successfully submitted data

--- src/component/Contact/ContactForm.js
+++ src/component/Contact/ContactForm.js
@@ -1,23 +1,83 @@
 import { Button, Input, Textarea } from "@material-tailwind/react";
+import axios from "axios";
+import { useState } from "react";
 
 const ContactForm = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const changeHandler = (event) => {
+    let id = event.target.id;
+    let value = event.target.value;
+
+    setInputs((prevState) => {
+      return { ...prevState, [id]: value };
+    });
+  };
+
+  const clearInputs = () => {
+    setInputs({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    sendDataToAPI();
+    clearInputs();
+    console.log(inputs);
+  };
+
+  const sendDataToAPI = async () => {
+    try {
+      const url = "https://sheetdb.io/api/v1/47bqu85rg0ngn";
+
+      await axios.post(url, inputs);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
-    <form>
-        <div>
-          <Input label="Full Name" size="lg" />
-        </div>
-        <div class="mt-8">
-          <Input label="Email" size="lg" />
-        </div>
-        <div class="mt-8">
-          <Textarea label="Message" size="lg" />
-        </div>
-        <div class="mt-8">
-          <Button type="submit" className="w-1/2">
-            Send Message
-          </Button>
-        </div>
-      </form>
+    <form onSubmit={submitHandler}>
+      <div>
+        <Input
+          label="Full Name"
+          size="lg"
+          id="name"
+          onChange={changeHandler}
+          value={inputs.name}
+        />
+      </div>
+      <div className="mt-8">
+        <Input
+          label="Email"
+          size="lg"
+          id="email"
+          onChange={changeHandler}
+          value={inputs.email}
+        />
+      </div>
+      <div className="mt-8">
+        <Textarea
+          label="Message"
+          size="lg"
+          id="message"
+          onChange={changeHandler}
+          value={inputs.message}
+        />
+      </div>
+      <div className="mt-8">
+        <Button type="submit" className="w-1/2">
+          Send Message
+        </Button>
+      </div>
+    </form>
   );
 };
 
